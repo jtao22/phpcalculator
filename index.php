d2add9ab891a002744d6dc07026a7fc4aaa82cf3
Jonathan Tao <68477691+jtao22@users.noreply.github.com>
2020-11-02 23:37:54 -0800

    created automatic class loader function

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+	spl_autoload_register('myAutoLoader');
+
+	function myAutoLoader($classname){
+		$path = "classes/";
+		$extension = ".class.php";
+		$fullPath = $path . $classname . $extension;
+
+		include_once $fullPath;
+	}
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 	<head>
